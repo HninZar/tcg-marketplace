13834009c1ededa864e680143462a092186d3fc9
Hnin Zar Khine <hninzarkhine@Hnins-MacBook-Air.local>
2023-10-01 18:42:36 +0700

    style(index):remove question mark

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ export default function Home() {
                             </Center>
                           </GridItem>
                         ))
-                      )} ? 
+                      )}
                     </Fragment>
                   )) }
               </Grid>
